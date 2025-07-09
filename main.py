67b2cd83848b1cd46c2818313cf5b834018f12b5
Thredo <pirizleonardod@gmail.com>
2025-07-09 19:29:18 -0300

    Added system prompts and function declaration

--- main.py
+++ main.py
@@ -3,14 +3,22 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-
-
+from functions.get_files_info import schema_get_files_info
+from prompts import system_prompt
 
 def main():
     load_dotenv()
+
+    #Variables
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     command_lst = ["--verbose"]
+    
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
 
     args = [m for m in sys.argv[1:] if m not in command_lst]
     opt_command = False
@@ -32,13 +40,19 @@ def main():
 
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
-        contents=messages
+        contents=messages,
+        config = types.GenerateContentConfig(
+            tools = [available_functions],
+            system_instruction = system_prompt
+        ),
+
     )
     if opt_command:
         print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(response.text)
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
 
 
 if __name__ == "__main__":
